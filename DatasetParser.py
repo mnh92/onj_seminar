696165668702689da6ec1b79bfc44760607ace38
Marko Novak Hindel <marko.n.hindel@gmail.com>
2017-01-14 19:26:44 +0100

    implemented punctuation parsing

--- DatasetParser.py
+++ DatasetParser.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 
 from bs4 import BeautifulSoup
+from nltk.tokenize import wordpunct_tokenize
 
 
 def parse_file(file):
@@ -9,14 +10,12 @@ def parse_file(file):
 
     for document in xml_tree.iterfind('documents/document'):
         tweets.append(__process_tweet(document.text))
-        break
-
     return tweets
 
 
-def __process_tweet(tweet_text):
+def __process_tweet(tweet_html):
     tweet_processed = {}
-    soup = BeautifulSoup(tweet_text, 'html.parser')
+    soup = BeautifulSoup(tweet_html, 'html.parser')
     all_links = soup.find_all('a')
     users, images, hashtags, links = __process_links(all_links)
     tweet_processed['users'] = 1.0 if len(users) > 0 else 0.0
@@ -24,6 +23,14 @@ def __process_tweet(tweet_text):
     tweet_processed['hashtags'] = 1.0 if len(hashtags) > 0 else 0.0
     tweet_processed['links'] = 1.0 if len(links) > 0 else 0.0
 
+    tweet_text = ''.join(soup.findAll(text=True))
+    dot_count, question_count, exclamation_count, ellipsis_count = __process_punctuation(tweet_text)
+    total_punctuation = dot_count + question_count + exclamation_count + ellipsis_count
+
+    tweet_processed['.'] = dot_count / total_punctuation if total_punctuation > 0 else 0.0
+    tweet_processed['?'] = question_count / total_punctuation if total_punctuation > 0 else 0.0
+    tweet_processed['!'] = exclamation_count / total_punctuation if total_punctuation > 0 else 0.0
+    tweet_processed['...'] = ellipsis_count / total_punctuation if total_punctuation > 0 else 0.0
     return tweet_processed
 
 
@@ -43,3 +50,24 @@ def __process_links(links):
         else:
             others.append(link)
     return users, images, hashtags, others
+
+
+def __process_punctuation(text):
+    dot_count = 0
+    question_count = 0
+    exclamation_count = 0
+    ellipsis_count = 0
+
+    tokens = wordpunct_tokenize(text)
+    for token in tokens:
+        if token == '.':
+            dot_count += 1
+        elif token.count('?') > 0:
+            question_count += 1
+        elif token.count('!') > 0:
+            exclamation_count += 1
+        elif token.count('.') > 1:
+            ellipsis_count += 1
+
+    return dot_count, question_count, exclamation_count, ellipsis_count
+
