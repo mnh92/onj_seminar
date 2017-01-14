327c88014c1aa9bc240c4949544e5c64dbc97396
Marko Novak Hindel <marko.n.hindel@gmail.com>
2017-01-14 17:11:25 +0100

    added example processed tweet

--- DatasetParser.py
+++ DatasetParser.py
@@ -1,12 +1,18 @@
 import xml.etree.ElementTree as ET
 
+from bs4 import BeautifulSoup
+
 
 def parse_file(file):
+    soup = BeautifulSoup()
     xml_tree = ET.parse(file)
     tweets = []
 
     for document in xml_tree.iterfind('documents/document'):
         tweet_text = document.text
+
+        print(tweet_text)
+
         tweets.append(tweet_text)
 
     return tweets
--- main.py
+++ main.py
@@ -4,3 +4,13 @@ import DatasetParser as dp
 with open('example_data/0a9e35fd6f123137d585a482f2484d8e.xml', 'r') as file:
     instances = dp.parse_file(file)
 
+# example processed tweet
+tweet_example = {}
+tweet_example['images'] = 0.0
+tweet_example['users'] = 1.0
+tweet_example['links'] = 1.0
+tweet_example['.'] = 0.0
+tweet_example['!'] = 0.5
+tweet_example['?'] = 0.0
+tweet_example['...'] = 0.34
+tweet_example['female'] = True
